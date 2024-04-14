c1cc7c07da801789cc0d99050688c28189c6360b
Clebson Moura <clebson.sh@gmail.com>
2024-04-14 13:10:08 -0300

    Rework on readability

--- install.sh
+++ install.sh
@@ -1,60 +1,70 @@
 #!/bin/bash
 set -e
-PHP_VERSION="8.3"
+
+echo "
+  Development Environment Setup for Full-Stack Laravel Development
+
+  This script facilitates the installation of the following components necessary for Full-Stack Laravel development:
+
+    PHP (choose from versions 8.3, 8.2, 8.1, 8.0, 7.4)
+    Composer
+    Nginx
+    MySQL
+    Laravel Valet
+    Laravel Installer
+    NVM (Node Version Manager)
+    Node.js
+    Yarn
+
+  Simply run the script to automate the setup process.
+"
+if ! [ -x "$(command -v php)" ]; then
+  echo "
+    Select a PHP version to install...
+  "
+  select PHP_VERSION in "8.3" "8.2" "8.1" "8.0" "7.4"
+  do
+    if ! [ -z "$PHP_VERSION" ]
+    then
+      echo "
+        php$PHP_VERSION will be installed...
+      "
+      break
+    fi
+  done
+fi
 
 if ! grep -q "^deb .*universe" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-  echo "adding universe repository..."
+  echo "
+    Adding universe repository...
+  "
   sudo add-apt-repository universe -y
 fi
 
 if ! grep -q "^deb .*ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-  echo "adding ondrej/php ppa repository..."
+  echo "
+    Adding ondrej/php ppa repository...
+  "
   sudo add-apt-repository ppa:ondrej/php -y
 fi
 
+echo "
+  Instaling system updates...
+"
 sudo apt update
 sudo apt upgrade -y
 
-if ! [ -x "$(command -v git)" ]; then
-  echo "installing git..."
-  sudo apt install git -y
-fi
+echo "
+  Instaling/updating Laravel Valet dependencies...
+  (git, vim, network, libnss3-tools, jq, xsel, curl, unzip)
+"
 
-if ! [ -x "$(command -v vim)" ]; then
-  echo "installing vim..."
-  sudo apt install vim -y
-fi
-
-if ! [ -x "$(command -v network-manager)" ]; then
-  sudo apt install network-manager -y
-fi
-
-if ! [ -x "$(command -v libnss3-tools)" ]; then
-  sudo apt install libnss3-tools -y
-fi
-
-if ! [ -x "$(command -v jq)" ]; then
-  echo "installing jq..."
-  sudo apt install jq -y
-fi
-
-if ! [ -x "$(command -v xsel)" ]; then
-  echo "installing xsel..."
-  sudo apt install xsel -y
-fi
-
-if ! [ -x "$(command -v curl)" ]; then
-  echo "installing curl..."
-  sudo apt install curl -y
-fi
-
-if ! [ -x "$(command -v unzip)" ]; then
-  echo "installing unzip..."
-  sudo apt install unzip -y
-fi
+sudo apt install git vim network-manager libnss3-tools jq xsel curl unzip -y
 
 if ! [ -x "$(command -v php)" ]; then
-  echo "installing PHP$PHP_VERSION..."
+  echo "
+    Installing PHP$PHP_VERSION and Laravel php dependecies...
+  "
   sudo apt install "php$PHP_VERSION-fpm" -y
   sudo apt install "php$PHP_VERSION" \
     "php$PHP_VERSION-cli" \
@@ -74,7 +84,9 @@ if ! [ -x "$(command -v php)" ]; then
 fi
 
 if ! [ -x "$(command -v composer)" ]; then
-  echo "installing Composer..."
+  echo "
+    Installing Composer...
+  "
   curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/
   sudo ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
   echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
@@ -82,30 +94,46 @@ if ! [ -x "$(command -v composer)" ]; then
 fi
 
 if ! [ -x "$(command -v valet)" ]; then
-  echo "installing Laravel Valet..."
+  echo "
+    Installing Laravel Valet...
+  "
   composer global require cpriego/valet-linux
   export PATH="$HOME/.config/composer/vendor/bin:$PATH"
   valet install
 fi
 
 if ! [ -x "$(command -v laravel)" ]; then
-  echo "installing Laravel Installer..."
+  echo "
+    Installing Laravel Installer...
+  "
   composer global require laravel/installer
 fi
 
 if ! [ -x "$(command -v mysql)" ]; then
-  echo "installing MySQL..."
+  echo "
+    Installing MySQL...
+  "
   sudo apt install mysql-server -y
+
+  echo "
+    Creating mysql user
+    user: sail
+    password: password
+  "
   sudo mysql -e "CREATE USER 'sail'@localhost IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON *.* TO 'sail'@localhost;FLUSH PRIVILEGES;"
 fi
 
 if ! [ -x "$(command -v redis-server)" ]; then
-  echo "installing Redis..."
+  echo "
+    Installing Redis...
+  "
   sudo apt install redis-server -y
 fi
 
 if ! [ -x "$(command -v node -v)" ]; then
-  echo "installing NVM, Node and Yarn..."
+  echo "
+    Installing NVM, Node and Yarn...
+  "
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -113,17 +141,4 @@ if ! [ -x "$(command -v node -v)" ]; then
   nvm install 20
   npm install -g yarn
   yarn config set -- --emoji true
-fi
-
-if ! [ -x "$(command -v code)" ]; then
-  echo "installing VSCode..."
-
-  sudo apt-get install gpg -y
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-  rm -f packages.microsoft.gpg
-  sudo apt install apt-transport-https -y
-  sudo apt update
-  sudo apt install code -y
 fi
