016779b6086462c70bd0aa95701934eded000649
Clebson Moura <clebson.sh@gmail.com>
2024-06-16 14:00:48 -0300

    bumo nvm version

--- setup.sh
+++ setup.sh
@@ -209,7 +209,7 @@ if ! [ -x "$(command -v node)" ]; then
   echo "
         Installing NVM, Node.js, and Yarn...
   "
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
