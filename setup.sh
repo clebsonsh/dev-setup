ed42998ab2fa3673f651d7bd49df809c74b417ff
Clebson Moura <clebson.sh@gmail.com>
2024-06-16 16:02:32 -0300

    fix permission dnied bug

--- setup.sh
+++ setup.sh
@@ -31,12 +31,9 @@ sudo apt upgrade -y
 echo "
         Installing basic tools...
 "
-mkdir -p ~/.local/bin
-wget -q -O - https://raw.githubusercontent.com/clebsonsh/dev-setup/main/add-site > ~/.local/bin/add-site
-chmod +x ~/.local/bin/add-site
-
-mkdir -p ~/.dev/sites-available
-mkdir -p ~/.dev/sites-enabled
+wget https://raw.githubusercontent.com/clebsonsh/dev-setup/main/add-site
+sudo mv add-site /usr/local/bin/add-site
+sudo chmod +x /usr/local/bin/add-site
 
 
 sudo apt install htop curl git vim unzip -y
@@ -51,108 +48,9 @@ echo "
         Installing NGINX
 "
 sudo apt install nginx -y
-sudo cat > /home/$USER/.dev/nginx.conf <<EOF
-user $USER;
-worker_processes auto;
-pid /run/nginx.pid;
-error_log /var/log/nginx/error.log;
-include /etc/nginx/modules-enabled/*.conf;
+sudo sed -i "s/user www-data/user $USER/" /etc/nginx/nginx.conf
 
-events {
-	worker_connections 768;
-	# multi_accept on;
-}
-
-http {
-
-	##
-	# Basic Settings
-	##
-
-	sendfile on;
-	tcp_nopush on;
-	types_hash_max_size 2048;
-	# server_tokens off;
-
-	# server_names_hash_bucket_size 64;
-	# server_name_in_redirect off;
-
-	include /etc/nginx/mime.types;
-	default_type application/octet-stream;
-
-	##
-	# SSL Settings
-	##
-
-	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
-	ssl_prefer_server_ciphers on;
-
-	##
-	# Logging Settings
-	##
-
-	access_log /var/log/nginx/access.log;
-
-	##
-	# Gzip Settings
-	##
-
-	gzip on;
-
-	# gzip_vary on;
-	# gzip_proxied any;
-	# gzip_comp_level 6;
-	# gzip_buffers 16 8k;
-	# gzip_http_version 1.1;
-	# gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
-
-	##
-	# Virtual Host Configs
-	##
-
-	include /etc/nginx/conf.d/*.conf;
-	include /home/$USER/.dev/sites-enabled/*;
-}
-EOF
-sudo cat > /etc/systemd/system/user-nginx.service <<EOF
-  # Stop dance for nginx
-# =======================
-#
-# ExecStop sends SIGQUIT (graceful stop) to the nginx process.
-# If, after 5s (--retry QUIT/5) nginx is still running, systemd takes control
-# and sends SIGTERM (fast shutdown) to the main process.
-# After another 5s (TimeoutStopSec=5), and if nginx is alive, systemd sends
-# SIGKILL to all the remaining processes in the process group (KillMode=mixed).
-#
-# nginx signals reference doc:
-# http://nginx.org/en/docs/control.html
-#
-[Unit]
-Description=A high performance web server and a reverse proxy server
-Documentation=man:nginx(8)
-After=network-online.target remote-fs.target nss-lookup.target
-Wants=network-online.target
-
-[Service]
-Type=forking
-PIDFile=/run/nginx.pid
-ExecStartPre=/usr/sbin/nginx -t -q -g 'daemon on; master_process on;'
-ExecStart=/usr/sbin/nginx -c /home/$USER/.dev/nginx.conf -g 'daemon on; master_process on;'
-ExecReload=/usr/sbin/nginx -c /home/$USER/.dev/nginx.conf -g 'daemon on; master_process on;' -s reload
-ExecStop=-/sbin/start-stop-daemon --quiet --stop --retry QUIT/5 --pidfile /run/nginx.pid
-TimeoutStopSec=5
-KillMode=mixed
-
-[Install]
-WantedBy=multi-user.target
-
-EOF
-sudo systemctl stop nginx
-sudo systemctl disable nginx
-
-sudo systemctl start user-nginx
-sudo systemctl enable user-nginx
-
+sudo systemctl restart nginx
 
 PHP_VERSION=8.3
 if ! [ -x "$(command -v php)" ]; then
