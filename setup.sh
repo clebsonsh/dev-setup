e4b1e15cefaa1403a30e1120654d462799b328b3
Clebson Moura <clebson.sh@gmail.com>
2025-07-17 21:56:54 -0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ set -e
 echo "
         This script automates the installation of the essential components required for Full-Stack Laravel development:
 
+          - Ubuntu 24.04
           - PHP 8.3
-          - Node 20
+          - Node 22
           - Composer
           - Nginx
           - MariaDB (user: root with no password)
@@ -20,10 +21,6 @@ sleep 5
 
 sudo add-apt-repository universe -y
 
-if ! grep -q "^deb .*ondrej/php" /etc/apt/sources.list.d/ondrej-ubuntu-php-jammy.list 2>/dev/null; then
-  sudo -S add-apt-repository ppa:ondrej/php -y
-fi
-
 echo "
         Updating system packages...
 "
@@ -33,12 +30,15 @@ sudo apt upgrade -y
 echo "
         Installing basic tools...
 "
-wget https://raw.githubusercontent.com/clebsonsh/dev-setup/main/add-site
-sudo mv add-site /usr/local/bin/add-site
-sudo chmod +x /usr/local/bin/add-site
+wget https://raw.githubusercontent.com/clebsonsh/dev-setup/main/addsite.sh
+sudo mv addsite.sh /usr/local/bin/addsite
+sudo chmod +x /usr/local/bin/addsite
+
+wget https://raw.githubusercontent.com/clebsonsh/dev-setup/main/rmsite.sh
+sudo mv rmsite.sh /usr/local/bin/rmsite
+sudo chmod +x /usr/local/bin/rmsite
 
 sudo apt install htop curl git vim unzip -y
-curl -s https://ohmyposh.dev/install.sh | bash -s
 
 echo "
         Installing NGINX
@@ -70,31 +70,6 @@ if ! [ -x "$(command -v php)" ]; then
     "php$PHP_VERSION-dev" \
     "php$PHP_VERSION-bcmath"
 fi
-
-# Add the signature to trust the Microsoft repo
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-
-# Add repo to apt sources
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
-
-# Install the driver
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
-source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
-
-sudo pecl install -f sqlsrv
-sudo pecl install -f pdo_sqlsrv
-
-printf "; priority=20\nextension=sqlsrv.so\n" >sqlsrv.ini
-sudo mv sqlsrv.ini /etc/php/8.3/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" >pdo_sqlsrv.ini
-sudo mv pdo_sqlsrv.ini /etc/php/8.3/mods-available/pdo_sqlsrv.ini
-sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
 
 sudo sed -i "s/www-data/$USER/" /etc/php/8.3/fpm/pool.d/www.conf
 
@@ -129,7 +104,7 @@ if ! [ -x "$(command -v node)" ]; then
   echo "
         Installing NVM, Node.js, and Yarn...
   "
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -137,7 +112,7 @@ if ! [ -x "$(command -v node)" ]; then
   echo "
         Installing Node 20...
   "
-  nvm install 20
+  nvm install 22
   npm install -g yarn
   yarn config set -- --emoji true
 fi
